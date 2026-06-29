42cca53f6742aad809a29cafee13247fd0cec6cf
TrailBlazer-1081729 <innovation1729@gmail.com>
2026-06-30 01:00:40 +0530

    Migrating to postgresql

--- database/db.py
+++ database/db.py
@@ -1,8 +1,15 @@
-import sqlite3
+import psycopg2
 import os
+from dotenv import load_dotenv
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_NAME = os.path.join(BASE_DIR, "leetcode.db")
+load_dotenv()
 
 def get_connection():
-    return sqlite3.connect(DB_NAME)
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT", "5432")
+    )
+
