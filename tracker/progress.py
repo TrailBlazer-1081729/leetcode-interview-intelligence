226145effef64a25e416a42a9033999092721a84
TrailBlazer-1081729 <innovation1729@gmail.com>
2026-06-30 15:33:58 +0530

    "perf: optimize progress syncing and update speed"

--- tracker/progress.py
+++ tracker/progress.py
@@ -1,72 +1,27 @@
 from database.db import get_connection
-def mark_solved(user_id,problem_id):
-    conn=get_connection()
-    cursor=conn.cursor()
-
-    try:
-        cursor.execute(
-            """INSERT INTO user_solved_problems(user_id,problem_id) VALUES(%s,%s) ON CONFLICT DO NOTHING""",
-            (user_id, problem_id))
-        conn.commit()
-    except Exception as e:
-        conn.rollback()
-        print(e)
-    finally:
-        conn.close()
-
-def unmark_solved(user_id, problem_id):
-    conn = get_connection()
-    cursor = conn.cursor()
-    try:
-        cursor.execute("""
-            DELETE FROM user_solved_problems
-            WHERE user_id=%s AND problem_id=%s
-            """, (user_id, problem_id))
-        conn.commit()
-    except Exception as e:
-        conn.rollback()
-        print(e)
-    finally:
-        conn.close()
-def get_solved(user_id):
-    conn=get_connection()
-    cursor=conn.cursor()
-    try:
-        cursor.execute("""SELECT problem_id
-            FROM user_solved_problems
-            WHERE user_id=%s""", (user_id,))
-    except Exception as e:
-        conn.rollback()
-        print(e)
-    solved=[row[0] for row in cursor.fetchall()]
-    conn.close()
-    return solved
-
-def sync_solved_problems(user_id, selected_problem_ids):
+def sync_solved_problems(user_id, selected_problem_ids, visible_problem_ids):
     conn = get_connection()
     cursor = conn.cursor()
 
     try:
         cursor.execute("""
             DELETE FROM user_solved_problems
-            WHERE user_id = %s
-            """, (user_id,))
-    except Exception as e:
-        conn.rollback()
-        print(e)
+            WHERE user_id = %s AND problem_id = ANY(%s)
+        """, (user_id, visible_problem_ids))
 
-
-    for problem_id in selected_problem_ids:
-        try:
+        for problem_id in selected_problem_ids:
             cursor.execute("""
-                    INSERT INTO user_solved_problems(user_id, problem_id)
-                    VALUES (%s, %s) ON CONFLICT DO NOTHING
-                    """, (user_id, problem_id))
-        except Exception as e:
-            conn.rollback()
-            print(e)
+                INSERT INTO user_solved_problems(user_id, problem_id)
+                VALUES (%s, %s)
+                ON CONFLICT DO NOTHING
+            """, (user_id, problem_id))
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        print(e)
+
+    finally:
+        conn.close()
 
 
